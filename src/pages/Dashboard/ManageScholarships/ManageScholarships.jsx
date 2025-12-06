22fb60e9e8c151a5b4d6e236a41889987a407fb0
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-12-07 00:00:06 +0600

    after delete scholarship handler

--- src/pages/Dashboard/ManageScholarships/ManageScholarships.jsx
+++ src/pages/Dashboard/ManageScholarships/ManageScholarships.jsx
@@ -3,11 +3,16 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
 import { Link } from "react-router";
+import Swal from "sweetalert2";
 
 const ManageScholarships = () => {
   const axiosSecure = useAxiosSecure();
 
-  const { data: scholarships = [], isLoading } = useQuery({
+  const {
+    data: scholarships = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["all-scholarships"],
     queryFn: async () => {
       const res = await axiosSecure.get("/all-scholarships");
@@ -15,13 +20,43 @@ const ManageScholarships = () => {
     },
   });
 
+    //delete handler
+  const deleteBtnHandler = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await axiosSecure.delete(`/scholarships/${id}`);
+
+          if (res.data.deletedCount > 0) {
+            Swal.fire("Deleted!", "Scholarship has been deleted.", "success");
+
+            refetch();
+          }
+        } catch (error) {
+          console.error("Error deleting scholarship:", error);
+          Swal.fire("Error", "Failed to delete scholarship!", "error");
+        }
+      }
+    });
+  };
+
   if (isLoading) {
     return <LoadingSpinner></LoadingSpinner>;
   }
 
   return (
     <div className="p-6">
-          <h2 className="text-2xl font-bold mb-6">Manage Scholarships-({scholarships.length })</h2>
+      <h2 className="text-2xl font-bold mb-6">
+        Manage Scholarships-({scholarships.length})
+      </h2>
       <div className="overflow-x-auto">
         <table className="min-w-full border border-gray-200 rounded-lg">
           <thead className="bg-gray-100">
@@ -45,10 +80,16 @@ const ManageScholarships = () => {
                   {new Date(sch.applicationDeadline).toLocaleDateString()}
                 </td>
                 <td className="py-3 px-4 flex gap-2">
-                  <Link to={`/dashboard/update-scholarship/${sch._id}`} className="btn btn-sm btn-warning flex items-center gap-1">
+                  <Link
+                    to={`/dashboard/update-scholarship/${sch._id}`}
+                    className="btn btn-sm btn-warning flex items-center gap-1"
+                  >
                     <FaEdit /> Update
                   </Link>
-                  <button className="btn btn-sm btn-error flex items-center gap-1">
+                  <button
+                    onClick={() => deleteBtnHandler(sch._id)}
+                    className="btn btn-sm btn-error flex items-center gap-1"
+                  >
                     <FaTrash /> Delete
                   </button>
                 </td>
